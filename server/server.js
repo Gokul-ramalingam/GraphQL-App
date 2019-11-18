79a7d0404b1df235d2965aa5d8115a8a5b2fb064
Gokul <gokulramalingam1@gmail.com>
2019-11-18 06:46:13 +0530

    Updation in server

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 
 const app = express()
-
+const schema = require('./schema/schema')
 const port = process.env.PORT || 6000
 
 
 app.use('/graphql',graphqlHTTP({
-    
+schema,
+graphiql : true
 }))
 
 app.listen(port,() =>{
