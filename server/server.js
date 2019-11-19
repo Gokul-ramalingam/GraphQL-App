d0ace9927c3b2d3769aa60be458883cf743cfce4
Gokul <gokulramalingam1@gmail.com>
2019-11-19 13:41:51 +0530

    Addition of mongo connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
-
+const url = require('./config/config').url;
 const app = express()
 
 const port = process.env.PORT || 7000
+
+
+
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true})
+                    .then(()=>console.log("Connected to mongoDB successfully..."))
+                    .catch((err) => console.log("Error occurred while connecting to mongoDB "+err))
+
 
 app.use('/graphql',graphqlHTTP({
 schema,
