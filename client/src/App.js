bedb2e0fde98bae493f35f9a4f8042c7cf987b37
Gokul <gokulramalingam1@gmail.com>
2019-11-20 22:48:30 +0530

    Update in app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import BookList from './component/BookList';
+
+//Apollo Client Setup
+const client = new ApolloClient({
+  uri: 'http://localhost:7000/graphql'
+})
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div id="main">
-      
+         <BookList/>
     </div>
+    </ApolloProvider>
   );
 }
 
